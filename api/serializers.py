cf141db5c719dab020472315fd8ef0787f494cea
Isabela Ribeiro <isabela.ribeiro1211@gmail.com>
2021-11-03 16:11:43 +0000

    feat - new changes

--- api/serializers.py
+++ api/serializers.py
@@ -11,12 +11,26 @@ class PackageSerializer(serializers.ModelSerializer):
         extra_kwargs = {"version": {"required": False}}
 
     def validate(self, data):
-        # TODO
-        # Validar o pacote, checar se ele existe na versão especificada.
-        # Buscar a última versão caso ela não seja especificada pelo usuário.
-        # Subir a exceção `serializers.ValidationError()` se o pacote não
-        # for válido.
-        return data
+
+        name = data.get('name', '')
+        version = data.get('version', '')
+
+        if version:
+            exist = version_exists(name, version)
+
+            if exist:
+                return data
+            else:
+                raise serializers.ValidationError({"error": "One or more packages doesn't exist"})
+        else:
+            latest = latest_version(name)
+
+            if latest:
+                data['version'] = latest
+                return data
+            else:
+                raise serializers.ValidationError({"error": "One or more packages doesn't exist"})
+        
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -27,11 +41,14 @@ class ProjectSerializer(serializers.ModelSerializer):
     packages = PackageSerializer(many=True)
 
     def create(self, validated_data):
-        # TODO
-        # Salvar o projeto e seus pacotes associados.
-        #
-        # Algumas referência para uso de models do Django:
-        # - https://docs.djangoproject.com/en/3.2/topics/db/models/
-        # - https://www.django-rest-framework.org/api-guide/serializers/#saving-instances
+
         packages = validated_data["packages"]
-        return Project(name=validated_data["name"])
+
+        projeto = Project.objects.create(name=validated_data["name"])  
+
+        for package in packages:
+            PackageRelease.objects.create(project=projeto, name=package['name'], version=package['version'])
+
+
+        return projeto
+       
